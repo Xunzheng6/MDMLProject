3ddc0774aabb5dd302ba22eb719b1fecd67896d2
Xunzheng6 <xunqing.zheng@gmail.com>
2024-04-26 08:07:49 -0400

    added new race col dropped NA row for anyone did not complete Kessler 6 parsed out num category for some predictors, such as income, urbanity, etc cleaned all question columns to just numbers, reverse some scale if needed calculated summary score for mental health,life satisfaction, soscial wellbeing, social support, identity, everyday discrimination, negative future expectation, community connectedness, and non-affirmation of gender identity

--- DataCleaning.R
+++ DataCleaning.R
@@ -1,7 +1,7 @@
 library(readr)
 library(dplyr)
 library(stringr) 
-
+library(tidyr)
 
 ##loading the raw data
 TransPop = read_csv("TransPop.csv")
@@ -12,7 +12,7 @@ CISgender = read_csv("CIS.csv")
 TransPopNew = TransPop %>% 
   ##selecting the variable we need
   ##recorganizing the data for easier merge with the CIS data
-  select(STUDYID,WEIGHT,GRESPONDENT_DATE,RACE,SEXUALID,SEX,AGE,GENDER,HINC,PINC,
+  select(STUDYID,WEIGHT,GRESPONDENT_DATE,RACE,RACE_RECODE,RACE_RECODE_CAT5,SEXUALID,SEX,AGE,GENDER,HINC,PINC,
          GEDUC1,GEDUCATION,GEMPLOYMENT2010,GURBAN,POVERTYCAT,GANN_INC,GCENREG,
          GCENDIV,GMILESAWAY,
          ##Kessler-6 score
@@ -38,7 +38,7 @@ TransPopNew = TransPop %>%
 CISdata = CISgender %>% 
   ##selecting the variable we need
   ##recorganizing the data for easier merge with the transpop data
-  select(STUDYID,WEIGHT,GRESPONDENT_DATE,RACE,SEXUALID,SEX,AGE,GENDER,HINC,PINC,
+  select(STUDYID,WEIGHT,GRESPONDENT_DATE,RACE,RACE_RECODE,RACE_RECODE_CAT5,SEXUALID,SEX,AGE,GENDER,HINC,PINC,
          GEDUC1,GEDUCATION,GEMPLOYMENT2010,GURBAN,POVERTYCAT,GANN_INC,GCENREG,
          GCENDIV,GMILESAWAY,
          ##Kessler-6 score
@@ -81,6 +81,14 @@ CISdata = CISgender %>%
 ##Combining the data for next step cleaning
 Combined = rbind(TransPopNew,CISdata)
 
+##Checking & dropping NA (Original N=1436)
+## 48 dropped due to incomplete Kessler Q, 1 from trans, 47 from cis
+sum(is.na(Combined$Q100A))
+NAcheck = filter(Combined,is.na(Q100A))
+Combined = Combined %>% 
+  drop_na(Q100A,Q100B,Q100C,Q100D,Q100E,Q100F)
+table(Combined$Data_source)
+
 ##Getting initial list of column name for data dictionary creation
 colnames(Combined)
 
@@ -109,7 +117,7 @@ Combined = Combined %>%
 table(Combined$Gender_text)
 table(Combined$Gender_num)
 
-##splitting numbers category from text 
+## splitting numbers category from text 
 Combined = Combined %>% 
   mutate(SEXUALID_num = parse_number(SEXUALID),
          HINC_num = parse_number(HINC),
@@ -119,18 +127,125 @@ Combined = Combined %>%
          INC_Bracket_num = parse_number(GANN_INC),
          Urban_num = parse_number(GURBAN))
 
-##Spliting numbers and text content into to column for race, sex, urbanity, etc
-
-##clean the quesion columns to just numbers for easier analysis
-## creating sum columns for outcome variables
-
-##Creating our own gender column
+## clean the question columns to just numbers for easier analysis
 Combined = Combined %>% 
-  mutate(Gender_2 = paste(Data_source,GENDER))
+  mutate(##Kessler-6 score
+    Q100A = parse_number(Q100A), Q100B = parse_number(Q100B), Q100C = parse_number(Q100C),
+    Q100D = parse_number(Q100D), Q100E = parse_number(Q100E), Q100F = parse_number(Q100F),
+    Q100A = case_when(Q100A==5 ~ 1, Q100A==4 ~ 2, Q100A==2 ~ 4, Q100A==1 ~ 5, Q100A==3 ~ 3),
+    Q100B = case_when(Q100B==5 ~ 1, Q100B==4 ~ 2, Q100B==2 ~ 4, Q100B==1 ~ 5, Q100B==3 ~ 3),
+    Q100C = case_when(Q100C==5 ~ 1, Q100C==4 ~ 2, Q100C==2 ~ 4, Q100C==1 ~ 5, Q100C==3 ~ 3),
+    Q100D = case_when(Q100D==5 ~ 1, Q100D==4 ~ 2, Q100D==2 ~ 4, Q100D==1 ~ 5, Q100D==3 ~ 3),
+    Q100E = case_when(Q100E==5 ~ 1, Q100E==4 ~ 2, Q100E==2 ~ 4, Q100E==1 ~ 5, Q100E==3 ~ 3),
+    Q100F = case_when(Q100F==5 ~ 1, Q100F==4 ~ 2, Q100F==2 ~ 4, Q100F==1 ~ 5, Q100F==3 ~ 3),
+    ##satisfaction with life
+    Q224 = parse_number(Q224), Q225 = parse_number(Q225), Q226 = parse_number(Q226),
+    Q227 = parse_number(Q227), Q228 = parse_number(Q228),
+    ##social well-being
+    Q04 = parse_number(Q04), Q05 = parse_number(Q05), Q06 = parse_number(Q06), 
+    Q07 = parse_number(Q07), Q08 = parse_number(Q08), Q09 = parse_number(Q09), 
+    Q10 = parse_number(Q10), Q11 = parse_number(Q11), Q12 = parse_number(Q12), 
+    Q13 = parse_number(Q13), Q14 = parse_number(Q14), Q15 = parse_number(Q15),
+    Q16 = parse_number(Q16), Q17 = parse_number(Q17), Q18 = parse_number(Q18),
+    Q04 = case_when(Q04==7 ~ 1, Q04==6 ~ 2, Q04==5 ~ 3, Q04==4 ~ 4, Q04==3 ~ 5,
+                    Q04==2 ~ 6, Q04==1 ~ 7),
+    Q08 = case_when(Q08==7 ~ 1, Q08==6 ~ 2, Q08==5 ~ 3, Q08==4 ~ 4, Q08==3 ~ 5,
+                    Q08==2 ~ 6, Q08==1 ~ 7),
+    Q11 = case_when(Q11==7 ~ 1, Q11==6 ~ 2, Q11==5 ~ 3, Q11==4 ~ 4, Q11==3 ~ 5,
+                    Q11==2 ~ 6, Q11==1 ~ 7),
+    Q12 = case_when(Q12==7 ~ 1, Q12==6 ~ 2, Q12==5 ~ 3, Q12==4 ~ 4, Q12==3 ~ 5,
+                    Q12==2 ~ 6, Q12==1 ~ 7),
+    Q14 = case_when(Q14==7 ~ 1, Q14==6 ~ 2, Q14==5 ~ 3, Q14==4 ~ 4, Q14==3 ~ 5,
+                    Q14==2 ~ 6, Q14==1 ~ 7),
+    Q15 = case_when(Q15==7 ~ 1, Q15==6 ~ 2, Q15==5 ~ 3, Q15==4 ~ 4, Q15==3 ~ 5,
+                    Q15==2 ~ 6, Q15==1 ~ 7),
+    Q16 = case_when(Q16==7 ~ 1, Q16==6 ~ 2, Q16==5 ~ 3, Q16==4 ~ 4, Q16==3 ~ 5,
+                    Q16==2 ~ 6, Q16==1 ~ 7),
+    Q17 = case_when(Q17==7 ~ 1, Q17==6 ~ 2, Q17==5 ~ 3, Q17==4 ~ 4, Q17==3 ~ 5,
+                    Q17==2 ~ 6, Q17==1 ~ 7),
+    ##identity
+    Q22 = parse_number(Q22), Q23 = parse_number(Q23), Q24 = parse_number(Q24),
+    Q25 = parse_number(Q25), Q26 = parse_number(Q26), Q27 = parse_number(Q27),
+    ##Everyday discrimination
+    Q171A = parse_number(Q171A), Q171B = parse_number(Q171B), Q171C = parse_number(Q171C),
+    Q171D = parse_number(Q171D), Q171E = parse_number(Q171E), Q171F = parse_number(Q171F), 
+    Q171G = parse_number(Q171G), Q171H = parse_number(Q171H), Q171I = parse_number(Q171I),
+    ##Social Support
+    Q192A = parse_number(Q192A), Q192B = parse_number(Q192B), Q192C = parse_number(Q192C), 
+    Q192D = parse_number(Q192D), Q192E = parse_number(Q192E), Q192F = parse_number(Q192F),
+    Q192G = parse_number(Q192G), Q192H = parse_number(Q192H), Q192I = parse_number(Q192I),
+    Q192J = parse_number(Q192J), Q192K = parse_number(Q192K), Q192L = parse_number(Q192L),
+    ##Future Expectations, Transpop Only
+    Q191A = parse_number(Q191A), Q191B = parse_number(Q191B), Q191C = parse_number(Q191C), 
+    Q191D = parse_number(Q191D), Q191E = parse_number(Q191E), Q191F = parse_number(Q191F),
+    Q191G = parse_number(Q191G), Q191H = parse_number(Q191H), Q191I = parse_number(Q191I),
+    ##Community Connectedness, Transpop Only
+    Q51 = parse_number(Q51), Q52 = parse_number(Q52), Q53 = parse_number(Q53),
+    Q54 = parse_number(Q54), Q55 = parse_number(Q55),
+    Q54 = case_when(Q54==5 ~ 1, Q54==4 ~ 2, Q54==2 ~ 4, Q54==1 ~ 5, Q54==3 ~ 3),
+    Q55 = case_when(Q55==5 ~ 1, Q55==4 ~ 2, Q55==2 ~ 4, Q55==1 ~ 5, Q55==3 ~ 3),
+    ##Non-Affirmation of Gender Identity, Transpop Only
+    Q45 = parse_number(Q45), Q46 = parse_number(Q46), Q47 = parse_number(Q47), 
+    Q48 = parse_number(Q48), Q49 = parse_number(Q49), Q50 = parse_number(Q50))
 
-data.frame(table(Combined$Gender_2))
+## mental health score scaled to 24
+Combined = Combined %>% 
+  mutate(Kessler6 = (Q100A+Q100B+Q100C+Q100D+Q100E+Q100F)/30*24)
+
+## satisfaction with life | 18 missing
+LifeSat = Combined %>% 
+  select(Q224,Q225,Q226,Q227,Q228)
+Combined$LifeSat = rowMeans(LifeSat,na.rm = TRUE)
+
+## social well being | 1 missing
+Socialwb = Combined %>% 
+  select(Q04,Q05,Q06,Q07,Q08,Q09,Q10,Q11,Q12,Q13,Q14,Q15,Q16,Q17,Q18)
+Combined$Socialwb = rowMeans(Socialwb,na.rm = TRUE)
+
+##identity | 12 missing
+MultiEthnicID = Combined %>% 
+  select(Q22,Q23,Q24,Q25,Q26,Q27)
+Combined$MultiEthnicID = rowMeans(MultiEthnicID,na.rm = TRUE)
+
+##Everyday discrimination | 21 missing
+Every_dis = Combined %>% 
+  select(Q171A,Q171B,Q171C,Q171D,Q171E,Q171F,Q171G,Q171H,Q171I)
+Combined$Every_dis = rowMeans(Every_dis,na.rm = TRUE)
+
+##Social Support | 23 missing
+Socialsup = Combined %>% 
+  select(Q192A,Q192B,Q192C,Q192D,Q192E,Q192F,Q192G,Q192H,Q192I,Q192J,Q192K,Q192L)
+Combined$Socialsup = rowMeans(Socialsup,na.rm = TRUE)
+
+##Social Support - family | 24 missing
+SocialsupFam = Combined %>% 
+  select(Q192C,Q192D,Q192H,Q192K)
+Combined$SocialsupFam = rowMeans(SocialsupFam,na.rm = TRUE)
+
+##Social Support - friend | 23 missing
+SocialsupFriend = Combined %>% 
+  select(Q192F,Q192G,Q192I,Q192L)
+Combined$SocialsupFriend = rowMeans(SocialsupFriend,na.rm = TRUE)
+
+##Future Expectations, Transpop Only
+NegExp = Combined %>% 
+  select(Q191A,Q191B,Q191C,Q191D,Q191E,Q191F,Q191G,Q191H,Q191I)
+Combined$NegExp = rowMeans(NegExp,na.rm = TRUE)
+
+##Community Connectedness, Transpop Only
+ComCon = Combined %>% 
+  select(Q51,Q52,Q53,Q54,Q55)
+Combined$ComCon = rowMeans(ComCon,na.rm = TRUE)
+
+##Non-Affirmation of Gender Identity, Transpop Only
+NonAffirm = Combined %>% 
+  select(Q45,Q46,Q47,Q48,Q49,Q50)
+Combined$NonAffirm = rowMeans(NonAffirm,na.rm = TRUE)
+
 
 ## Creating Race gender intersection
+sum(is.na(Combined$RACE))
+
 
 ##creating regional & divisional scores based on the state-level friendiness score
 ##connect two datasets together
